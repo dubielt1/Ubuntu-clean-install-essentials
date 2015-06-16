a19316d2f80bc36a7ae27bfa03f047a741395a5c
Tres DuBiel <dubielt1@gmail.com>
2015-06-16 14:10:25 -0500

    Updated gcc

--- instructions.sh
+++ instructions.sh
@@ -8,6 +8,11 @@ System
   sudo apt-get install curl
   sudo apt-get install vim
   
+Latest version of gcc https://launchpad.net/~ubuntu-toolchain-r/+archive/ubuntu/test
+  sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+  sudo apt-get update
+  sudo apt-get install g++-4.9 [5.0 has been released]
+  
 Java Oracle - http://www.webupd8.org/2012/09/install-oracle-java-8-in-ubuntu-via-ppa.html
   sudo add-apt-repository ppa:webupd8team/java
   sudo apt-get update
